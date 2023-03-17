0ad53a2bab6e714fead33437a4f3f917a661fd64
Ferny <fernc28@outlook.com>
2023-03-16 22:54:49 -0400

    CSS styles and cards.js uddated

---
+++ Assets/js/cards.js
@@ -0,0 +1,16 @@
+var cards = [
+    {
+      question: 'What should the first paragraph of your essay be?',
+      answer: 'Intro, Conclusion, body, Information,'
+    },
+    {
+      question: 'To rewrite another writers words and ideas into your own words?',
+      answer: 'Summarize, Paraphrase, Plagiarize, All of the above'
+    },
+    {
+      question: 'What is the largest U.S. state by size?',
+      answer: 'Alask, Texas, California'
+    }
+  ];
+
+
